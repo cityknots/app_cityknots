a5e2e08abc4b1b7ad85a633e7780a686cf13207a
Carlos Cruz <cruz5114@gmail.com>
2015-11-05 16:29:30 -0600

    update parse credencial system

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,11 +3,44 @@
 */
 import Slider from './slider';
 import React from 'react';
+
+import Parse_Init from "./parse_start";
 import Parse from 'parse';
 import ParseReact from 'parse-react';
 
 // Init slider plug-in
 Slider();
+
+
+class CommentBlock extends React.Component{
+  observe() {
+  	console.log("hi")
+    // Subscribe to all Comment objects, ordered by creation date
+    // The results will be available at this.data.comments
+    return {
+      comments: (new Parse.Query('list')).ascending('createdAt')
+    };
+  }
+
+  render() {
+  	console.log(this.data)
+    // Render the text of each comment as a list item
+    return (
+      <ul>
+        {this.data.comments.map(function(c) {
+          return <li>{c.text}</li>;
+        })}
+      </ul>
+    );
+  }
+};
+
+React.render(
+  <CommentBlock />,
+  document.getElementById('main')
+);
+
+
 
 
 
---
+++ src/javascript/parse_start.js
@@ -0,0 +1,11 @@
+import Parse from 'parse';
+// import DataEnv from 'dotenv';
+
+function init(){
+	// DataEnv.load();
+	// Parse.initialize("process.env.Parse_APPID", "process.env.Parse_JSKEY");
+	Parse.initialize("Yl7PNaBFgL8ETlknO5zcr0Yk94BA0wlIPpmLBjVL", "rwWWFdljUZmbYHPPP43br4zitkbgnGxFzejXCEw1");
+
+}
+
+export default init();
