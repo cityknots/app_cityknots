4c2ab8ec82e22f4362c6663deb8fca121537ab6d
Cruz14 <cruz5114@gmail.com>
2015-11-04 23:18:41 -0600

    feat(parse): add parse integrations libs

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,18 @@
+/*
+*	Module dependencies
+*/
+import Slider from './slider';
 import React from 'react';
+import Parse from 'parse';
+import ParseReact from 'parse-react';
 
-console.log(React)
+// Init slider plug-in
+Slider();
+
+
+
+
+
+
+
+
---
+++ src/javascript/slider.js
@@ -0,0 +1,6 @@
+var slider1 = null;
+function init() {
+    slider1 = new lepsSlider();
+}
+
+export default init;
